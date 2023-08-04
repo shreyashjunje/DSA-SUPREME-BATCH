0ebe59a4f2ba6a3b2cb148046d11173fc7cda92e
shreyashjunje <shreyashjunje2003@gmail.com>
2023-08-04 23:18:32 +0530

    practised questions

--- char array and strins/practise.cpp
+++ char array and strins/practise.cpp
@@ -1,92 +1,106 @@
-
 #include<iostream>
-#include<string>
+#include<algorithm>
+#include<string.h>
 using namespace std;
 
-bool isValid(string s) {
-        string ans=" ";
-        int i=0;
-        while(i<s.length()){
-            if(s.length()<=1){
-                ans.push_back(s[i]);
-            }else{
-                if(ans[ans.length()-1]!=s[i]+2){
-                    return false;
-                }else{
-                    return true;
-                }
-                i++;
-            }
-            
+//find length of string using while loop
+int lengthofstring(char ch[100]){
+    int count=0;
+    int i=0;
+    while(ch[i]!='\0'){
+        count++;
+        i++;
+    }
+    return count;
+}
+
+//find length of string using for loop
+int lengthofstr(char ch[100]){
+    int count=0;
+    for(int i=0;i<strlen(ch);i++){
+        count++;
+    }
+    return count;
+}
+
+//reverse a string
+char reversestring(char ch[100]){
+    int s=0;
+    int e=strlen(ch)-1;
+    while(s<=e){
+        swap(ch[s],ch[e]);
+        s++;
+        e--;
+    }
+    cout<<ch;
+}
+
+char reverseelement(char ch[100]){
+    for(int i=strlen(ch);i>=0;i--){
+        cout<<ch[i];
+    }
+   
+}
+
+//replace a space by a character
+char replacespace(char ch[100]){
+    for(int i=0;i<strlen(ch);i++){
+        if(ch[i]==' '){
+            ch[i]='@';
         }
+    }
+    cout<<ch;
+}
+
+//palindrome
+bool palindrome(char ch[100]){
+    
+    int i=0;
+    int j=strlen(ch)-1;
+    while(ch[i]<=ch[j]){
+        if(ch[i]==ch[j]){
         
-    }
-    
-    int main(){
-        string s,ans;
-        cin>>s;
-        if(isValid(s)){
-            cout<<"yes";
-        }else{
-            cout<<"no";
+            return true;
+
         }
+            
+        
+        i++;
+         return false;
+    }
+   
+}
+
+int main(){
+    char ch[100];
+    cin.getline(ch,20);
+
+    for(int i=0;i<9;i++){
+        cout<<"value is "<<ch[i]<<" at index "<<i<<endl;
+
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include<iostream>
-// #include<string>
-// using namespace std;
-
-// int main(){
-//     //declare a string
-//     string str;
-//     string str1="yash";
-//     // string target="hello";
-//     //input
-//     getline(cin,str);
-//     //utput
-//     cout<<str<<endl;;
-//     cout<<"length of the string is "<<str.length()<<endl;
-//     cout<<"string is "<<str.empty()<<endl;
-//     str.push_back('g');
-//     cout<<str<<endl;
-//     str.pop_back();
-//     cout<<str<<endl;
-//     cout<<"substring: "<<str.substr(0,10)<<endl;
-//     if(str.compare(str1)==0){
-//         cout<<"strings are exactly same"<<endl;
-//     }else{
-//         cout<<"strings are not same"<<endl;
-//     }
-//     cout<<"compare: "<<str.compare(str1)<<endl;
-//     str.clear();
-//     cout<<"length of the string is "<<str.length()<<endl;
+    //find length of string
+    int findlengthofstring=lengthofstr(ch);
+    cout<<"length of the string is: "<<findlengthofstring;
+    cout<<endl;
     
-    
-//     return 0;
-// }
+    //reverse a string
+    cout<<"reverse of string is: ";
+    reverseelement(ch);
+    cout<<endl;
+
+    //replace spaces by a special character
+    cout<<"string after replacing spaces is: ";
+    replacespace(ch);
+    cout<<endl;
+
+
+    //palindrome or not
+    if(palindrome(ch)){
+        cout<<"it is a palindrome"<<endl;
+    }else{
+        cout<<"it is not palimdrome"<<endl;
+    }
+
+}
--- char array and strins/tempCodeRunnerFile.cpp
+++ char array and strins/tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
-
-    // getline(cin,str);
+ i++;
