359f41ed97bd070b773a743e0cc66dc1d844e83d
shreyashjunje <shreyashjunje2003@gmail.com>
2023-08-11 22:47:10 +0530

    practised string medium level problems

--- char array and strins/midlevelproblem.cpp
+++ char array and strins/midlevelproblem.cpp
@@ -21,11 +21,70 @@ string removeduplicates(string str){
         }
         return ans;
     }
-   
+
+//removeoccurrence of the string
+string removeoccurrences(string str, string part){
+    int pos = str.find(part);
+    while(pos != string::npos){
+        str.erase(pos, part.length());
+        pos=str.find(part);
+    }
+    return str;
+}
+
+//valid palindrome 
+
+bool checkpalindrome(string str,int j , int i){
+    while(i<=j){
+        if(str[i]==str[j]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+bool validpalindrome(string str){
+    int i=0;
+    int j=str.length()-1;
+    while(i<=j){
+        if(str[i]!=str[j]){
+            return checkpalindrome(str,i+1,j) || checkpalindrome(str,i,j-1);
+        }
+        else{
+            i++;
+            j--;
+        }
+    }
+    
+}
+
+
 
 int main(){
     string str;
     cout<<"enter the string: ";
     cin>>str;
+
+    string part;
+    cout<<"enter part: ";
+    cin>>part;
+
+    if(validpalindrome(str)){
+        cout<<"it is a palindrome after removing one character."<<endl;
+    }
+    else{
+         cout<<"it is not a palindrome after removing one character."<<endl;
+    }
+
+    string removeocc=removeoccurrences(str,part);
+    cout<<"string after removing all occurrences: "<<removeocc;
+    cout<<endl;
+
+    string a=removeduplicates(str);
+    cout<<"string after removing adjacent occurrences: "<<a;
+
+
 
 }
