59f700d1212bba9643ff0a50b8fc8f20070c97ad
shreyashjunje <shreyashjunje2003@gmail.com>
2023-08-14 00:21:43 +0530

    string

--- recursion/recursionlect1.cpp
+++ recursion/recursionlect1.cpp
@@ -1,4 +1,18 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
+
+string str(string str1,string str2){
+    return str1 + str2;
+}
+
+int main(){
+    string str1,str2;
+    cin>>str1;
+    cin>>str2;
+    string a=str(str1,str2);
+    cout<<a;
+
+}
 
 
