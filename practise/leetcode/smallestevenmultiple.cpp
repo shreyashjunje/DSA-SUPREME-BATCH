ac6e6e7be16cf9bf573406c46950b85eecff83bf
shreyashjunje <shreyashjunje2003@gmail.com>
2023-09-03 22:52:51 +0530

    .

---
+++ practise/leetcode/smallestevenmultiple.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int smallestevenmultiple(int n){
+    if(n%2==0)
+    return n;
+    else
+    return 2*n;
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int a=smallestevenmultiple(n);
+    cout<<a;
+}
+
