a1c54a0efc12402c59d99fba1bec2473f63aede4
shreyashjunje <shreyashjunje2003@gmail.com>
2024-01-21 23:26:28 +0530

    .

---
+++ REVISION/STACK/lect2/sort_stack.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void solve(stack<int>&st, int &target){
+    if(st.empty()){
+        st.push(target);
+        return;
+    }
+
+    if(st.top()>=target){
+        st.push(target);
+        return;
+    }
+
+    int temp=st.top();
+        st.pop();
+
+    solve(st,target);
+
+    st.push(temp);
+}
+
+
+void sortStack(stack<int>&st){
+    if(st.empty()){
+        return;
+    }
+
+    int target=st.top();
+    st.pop();
+
+    sortStack(st);
+
+    solve(st,target);
+}
+
+int main(){
+    stack<int>st;
+    st.push(10);
+    st.push(50);
+    st.push(5);
+    st.push(89);
+
+    sortStack(st);
+
+    while(!st.empty()){
+        cout<<st.top()<<endl;
+        st.pop();
+    }
+
+}
