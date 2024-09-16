8c000a97f59ac04cc9131f040d84afc0e3e7e3d8
shreyashjunje <shreyashjunje2003@gmail.com>
2024-09-16 23:31:13 +0530

    .

--- LECTURE/extra/patterns.cpp
+++ LECTURE/extra/patterns.cpp
@@ -1,134 +1,134 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
-    int rowCount,colCount;
-    cin>>rowCount;
+// int main(){
+//     int rowCount,colCount;
+//     cin>>rowCount;
 
     
-    //half pyramid
+//     //half pyramid
 
-       for(int i=0;i<rowCount;i++){
-        for(int j=0;j<i+1;j++){
-            cout<<"* ";
-        }
+//        for(int i=0;i<rowCount;i++){
+//         for(int j=0;j<i+1;j++){
+//             cout<<"* ";
+//         }
        
-        cout<<endl;
-    }
+//         cout<<endl;
+//     }
 
-    //inverted halpf pyramid
+//     //inverted halpf pyramid
 
-     for(int i=0;i<rowCount;i++){
-        for(int j=0;j<rowCount-i;j++){
-            cout<<"* ";
-        }
+//      for(int i=0;i<rowCount;i++){
+//         for(int j=0;j<rowCount-i;j++){
+//             cout<<"* ";
+//         }
        
-        cout<<endl;
-    }
+//         cout<<endl;
+//     }
 
 
-    // //numerical half pyramid
+//     // //numerical half pyramid
 
-       for(int i=0;i<rowCount;i++){
-        for(int j=0;j<i+1;j++){
-            cout<<j+1;
-        }
+//        for(int i=0;i<rowCount;i++){
+//         for(int j=0;j<i+1;j++){
+//             cout<<j+1;
+//         }
        
-        cout<<endl;
-    }
+//         cout<<endl;
+//     }
 
 
-    //inverted halpf pyramid
+//     //inverted halpf pyramid
     
-     for(int i=0;i<rowCount;i++){
-        for(int j=0;j<rowCount-i;j++){
-            cout<<j+1;
-        }
+//      for(int i=0;i<rowCount;i++){
+//         for(int j=0;j<rowCount-i;j++){
+//             cout<<j+1;
+//         }
        
-        cout<<endl;
-    }
+//         cout<<endl;
+//     }
 
-    //full pyramid
+//     //full pyramid
 
-    for(int i=0;i<1;i++){
+//     for(int i=0;i<1;i++){
         
-       for(int j=0;j<rowCount-i-1;j++){
-            cout<<" ";
-       }
+//        for(int j=0;j<rowCount-i-1;j++){
+//             cout<<" ";
+//        }
 
-       for(int j=0;j<i+1;j++){
-            cout<<"* ";
-       }
-       cout<<endl;
-    }
+//        for(int j=0;j<i+1;j++){
+//             cout<<"* ";
+//        }
+//        cout<<endl;
+//     }
 
 
-    // inverted full pyramid
+//     // inverted full pyramid
 
-    for(int i=0;i<rowCount;i++){
-        for(int j=0;j<i;j++){
-            cout<<" ";
-       }
-       for(int j=0;j<rowCount-i;j++){
-            cout<<"* ";
-       }
+//     for(int i=0;i<rowCount;i++){
+//         for(int j=0;j<i;j++){
+//             cout<<" ";
+//        }
+//        for(int j=0;j<rowCount-i;j++){
+//             cout<<"* ";
+//        }
 
        
-       cout<<endl;
-    }
+//        cout<<endl;
+//     }
     
 
-    //solid diamond
-    for(int i=0;i<rowCount;i++){
+//     //solid diamond
+//     for(int i=0;i<rowCount;i++){
         
-       for(int j=0;j<rowCount-i-1;j++){
-            cout<<" ";
-       }
+//        for(int j=0;j<rowCount-i-1;j++){
+//             cout<<" ";
+//        }
 
-       for(int j=0;j<i+1;j++){
-            cout<<"* ";
-       }
-       cout<<endl;
-    }
-    for(int i=0;i<rowCount;i++){
-        for(int j=0;j<i;j++){
-            cout<<" ";
-       }
-       for(int j=0;j<rowCount-i;j++){
-            cout<<"* ";
-       }
-       cout<<endl;
-    }
+//        for(int j=0;j<i+1;j++){
+//             cout<<"* ";
+//        }
+//        cout<<endl;
+//     }
+//     for(int i=0;i<rowCount;i++){
+//         for(int j=0;j<i;j++){
+//             cout<<" ";
+//        }
+//        for(int j=0;j<rowCount-i;j++){
+//             cout<<"* ";
+//        }
+//        cout<<endl;
+//     }
 
 
-    //hollow diamond
-    for(int i=0;i<rowCount;i++){
-       for(int j=0;j<rowCount-i-1;j++){
-        cout<<" ";
-       }
-       for(int j=0;j<2*i+1;j++){
-        if(j==0 || j==2*i){
-            cout<<"*";
-        }else{
-                cout<<" ";
-        }
-       }
-       cout<<endl;
-    }
+//     //hollow diamond
+//     for(int i=0;i<rowCount;i++){
+//        for(int j=0;j<rowCount-i-1;j++){
+//         cout<<" ";
+//        }
+//        for(int j=0;j<2*i+1;j++){
+//         if(j==0 || j==2*i){
+//             cout<<"*";
+//         }else{
+//                 cout<<" ";
+//         }
+//        }
+//        cout<<endl;
+//     }
      
-     for(int i=0;i<rowCount;i++){
-       for(int j=0;j<i;j++){
-        cout<<" ";
-       }
-       for(int j=0;j<2*rowCount-2*i-1;j++){
-        if(j==0 || j==2*rowCount-2*i-2){
-            cout<<"*";
-        }else{
-            cout<<" ";
-        }
-       }
+//      for(int i=0;i<rowCount;i++){
+//        for(int j=0;j<i;j++){
+//         cout<<" ";
+//        }
+//        for(int j=0;j<2*rowCount-2*i-1;j++){
+//         if(j==0 || j==2*rowCount-2*i-2){
+//             cout<<"*";
+//         }else{
+//             cout<<" ";
+//         }
+//        }
        
-       cout<<endl;
-    }
+//        cout<<endl;
+//     }
 
-}
+// }
