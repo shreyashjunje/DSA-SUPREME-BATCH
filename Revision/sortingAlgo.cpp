8517c39c6dcd551b50aba560864923477bbc4a31
shreyashjunje <shreyashjunje2003@gmail.com>
2024-09-23 23:39:18 +0530

    ..

--- Revision/sortingAlgo.cpp
+++ Revision/sortingAlgo.cpp
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void selectionSort(vector<int>&arr){
-    int n=arr.size();
-    for(int i=0;i<arr.size()-1;i++){
-        int minIndex=i;
-        for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[minIndex]){
-                minIndex=j;
-            }
-        }
-        swap(arr[minIndex],arr[i]);
-    }
-}
+// void selectionSort(vector<int>&arr){
+//     int n=arr.size();
+//     for(int i=0;i<arr.size()-1;i++){
+//         int minIndex=i;
+//         for(int j=i+1;j<n;j++){
+//             if(arr[j]<arr[minIndex]){
+//                 minIndex=j;
+//             }
+//         }
+//         swap(arr[minIndex],arr[i]);
+//     }
+// }
 
 int main(){
     vector<int>arr{5,6,2,3};
